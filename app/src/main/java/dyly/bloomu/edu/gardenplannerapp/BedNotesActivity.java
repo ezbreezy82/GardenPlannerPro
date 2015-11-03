4fc34dbe228d9e9b485b9ad4f212f5905e8a442f
EvanZimmerman <ewz23941@gmail.com>
2015-11-02 23:04:02 -0500

    Set up a bit of the logic to insert and create custom_bed_note views in the ListView for the bed notes.  Not close to being finished.

--- app/src/main/java/dyly/bloomu/edu/gardenplannerapp/BedNotesActivity.java
+++ app/src/main/java/dyly/bloomu/edu/gardenplannerapp/BedNotesActivity.java
@@ -4,17 +4,34 @@ import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class BedNotesActvity extends AppCompatActivity {
+public class BedNotesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bed_notes_actvity);
+
+        ListView noteList = (ListView) findViewById(R.id.noteList);
+        //create fake test entries.
+        View footView;
+        for (int i = 0; i < 5; i++)
+        {
+           footView = getLayoutInflater().inflate(R.layout.custom_bed_note, null);
+            noteList.addFooterView(footView);
+        }
+
     }
 
     @Override
