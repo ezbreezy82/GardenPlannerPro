7b23917d3d0e30f0459f9acc74c14676eb0d7b82
EvanZimmerman <ewz23941@gmail.com>
2015-11-06 12:51:50 -0500

    Custom view to be used as a row in the ListView for the notes section.

--- app/src/main/java/dyly/bloomu/edu/gardenplannerapp/CustomBedNotesHeader.java
+++ app/src/main/java/dyly/bloomu/edu/gardenplannerapp/CustomBedNotesHeader.java
@@ -1,7 +1,20 @@
 package dyly.bloomu.edu.gardenplannerapp;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+
 /**
  * Created by EVANDESKTOP on 11/5/2015.
  */
-public class CustomBedNotesHeader {
+public class CustomBedNotesHeader extends LinearLayout
+{
+
+    public CustomBedNotesHeader(Context context)
+    {
+        super(context, null);
+
+        final LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.custom_bed_notes_header, this);
+    }
 }
