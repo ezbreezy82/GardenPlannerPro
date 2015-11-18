acb89a5b1bbbb9aeac7d38facde9486228da8d83
EvanZimmerman <ewz23941@gmail.com>
2015-11-18 14:46:15 -0500

    Implemented the ListPopupWindow functionality which will show a drop down list when the user clicks on the noteControls allowing them to delete or modify that note entry.

--- app/src/main/java/dyly/bloomu/edu/gardenplannerapp/Adapters/BedLayoutNoteListAdapter.java
+++ app/src/main/java/dyly/bloomu/edu/gardenplannerapp/Adapters/BedLayoutNoteListAdapter.java
@@ -2,11 +2,16 @@ package dyly.bloomu.edu.gardenplannerapp.Adapters;
 
 import android.app.Activity;
 import android.content.Context;
+import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListPopupWindow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 
@@ -21,6 +26,8 @@ public class BedLayoutNoteListAdapter extends ArrayAdapter<Note> {
     Context context;
     int layoutResourceId;
     Note data[] = null;
+    private ListPopupWindow listPopupWindow;
+    private String[] controls = {"Edit", "Delete"};
 
     public BedLayoutNoteListAdapter(Context context, int layoutResourceId, Note[] data)
     {
@@ -31,7 +38,7 @@ public class BedLayoutNoteListAdapter extends ArrayAdapter<Note> {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(int position, final View convertView, ViewGroup parent) {
         View row = convertView;
         NoteHolder holder = null;
 
@@ -43,6 +50,7 @@ public class BedLayoutNoteListAdapter extends ArrayAdapter<Note> {
             holder = new NoteHolder();
             holder.subject = (TextView)row.findViewById(R.id.subject);
             holder.timeStamp = (TextView)row.findViewById(R.id.timeStamp);
+            holder.noteControls = (ImageButton)row.findViewById(R.id.noteControls);
 
             row.setTag(holder);
         }else
@@ -56,6 +64,31 @@ public class BedLayoutNoteListAdapter extends ArrayAdapter<Note> {
         //format date.
         String timeStampFormatted = new SimpleDateFormat("MM/dd/yyyy HH:mm").format(note.getTimeStamp());
         holder.timeStamp.setText(timeStampFormatted);
+        holder.noteControls.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listPopupWindow = new ListPopupWindow(getContext());
+                listPopupWindow.setAdapter(new ArrayAdapter<>(
+                        getContext(),
+                        R.layout.custom_bed_note_control_list_item,
+                        controls));
+                listPopupWindow.setAnchorView(v.findViewById(R.id.noteControls));
+                listPopupWindow.setWidth(100);
+                listPopupWindow.setHeight(120);
+                listPopupWindow.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        //if position == 1, Edit mode. redirect to edit the note.
+                        if(position == 0)
+                            redirectToEditNote();
+                        if(position == 1)
+                            requestDeletionOfNote();
+
+                    }
+                });
+                listPopupWindow.show();
+            }
+        });
 
         return row;
     }
@@ -64,6 +97,17 @@ public class BedLayoutNoteListAdapter extends ArrayAdapter<Note> {
     {
         TextView subject;
         TextView timeStamp;
+        ImageButton noteControls;
+    }
+
+    public void redirectToEditNote()
+    {
+        Toast.makeText(getContext(),"Coming soon",Toast.LENGTH_LONG).show();
+    }
+
+    public void requestDeletionOfNote()
+    {
+        Toast.makeText(getContext(),"Coming soon",Toast.LENGTH_LONG).show();
     }
 }
 
