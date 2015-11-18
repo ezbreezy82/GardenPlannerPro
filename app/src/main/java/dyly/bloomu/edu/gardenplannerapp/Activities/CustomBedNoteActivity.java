1584fae97ac3c655b59207b0974da636bcbb4a5b
EvanZimmerman <ewz23941@gmail.com>
2015-11-18 14:44:52 -0500

    Cleaned up file.

--- app/src/main/java/dyly/bloomu/edu/gardenplannerapp/Activities/CustomBedNoteActivity.java
+++ app/src/main/java/dyly/bloomu/edu/gardenplannerapp/Activities/CustomBedNoteActivity.java
@@ -1,7 +1,6 @@
 package dyly.bloomu.edu.gardenplannerapp.Activities;
 
 import android.content.Context;
-import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.LinearLayout;
 
