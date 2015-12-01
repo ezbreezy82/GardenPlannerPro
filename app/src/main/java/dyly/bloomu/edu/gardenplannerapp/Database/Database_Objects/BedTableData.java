1b554d07caa58ac62d60975ea39dbf9d10aeae1e
DW <Dylweiss@gmail.com>
2015-12-01 10:10:54 -0800

    removed unneeded values

--- app/src/main/java/dyly/bloomu/edu/gardenplannerapp/Database/Database_Objects/BedTableData.java
+++ app/src/main/java/dyly/bloomu/edu/gardenplannerapp/Database/Database_Objects/BedTableData.java
@@ -7,22 +7,13 @@ import java.io.Serializable;
  */
 public class BedTableData implements Serializable{
 
-    private int id, noteID, layoutID, imageID, historyID, gardenID;
+    private int id, gardenID;
 
     public BedTableData() {
-        this.id = -1;
-        this.noteID = -1;
-        this.layoutID = -1;
-        this.imageID = -1;
-        this.historyID = -1;
         this.gardenID = -1;
     }
 
-    public BedTableData(int noteID, int layoutID, int imageID, int historyID, int gardenID) {
-        this.noteID = noteID;
-        this.layoutID = layoutID;
-        this.imageID = imageID;
-        this.historyID = historyID;
+    public BedTableData( int gardenID) {
         this.gardenID = gardenID;
     }
 
@@ -32,38 +23,6 @@ public class BedTableData implements Serializable{
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public int getNoteID() {
-        return noteID;
-    }
-
-    public void setNoteID(int noteID) {
-        this.noteID = noteID;
-    }
-
-    public int getLayoutID() {
-        return layoutID;
-    }
-
-    public void setLayoutID(int layoutID) {
-        this.layoutID = layoutID;
-    }
-
-    public int getImageID() {
-        return imageID;
-    }
-
-    public void setImageID(int imageID) {
-        this.imageID = imageID;
-    }
-
-    public int getHistoryID() {
-        return historyID;
-    }
-
-    public void setHistoryID(int historyID) {
-        this.historyID = historyID;
     }
 
     public int getGardenID() {
