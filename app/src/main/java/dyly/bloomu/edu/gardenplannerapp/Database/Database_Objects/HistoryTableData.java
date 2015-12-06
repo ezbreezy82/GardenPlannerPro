1d7f96160f5c93df21227e88a6632f123bbe1ec3
EvanZimmerman <ewz23941@gmail.com>
2015-12-05 19:07:04 -0500

    extra commits from branch pr/4

--- app/src/main/java/dyly/bloomu/edu/gardenplannerapp/Database/Database_Objects/HistoryTableData.java
+++ app/src/main/java/dyly/bloomu/edu/gardenplannerapp/Database/Database_Objects/HistoryTableData.java
@@ -1,11 +1,17 @@
-package dyly.bloomu.edu.gardenplannerapp;
+package dyly.bloomu.edu.gardenplannerapp.Database.Database_Objects;
+
+import java.io.Serializable;
 
 /**
  * Created by Dyly on 11/4/15.
  */
-public class HistoryTableData {
+public class HistoryTableData implements Serializable {
 
+<<<<<<< HEAD:app/src/main/java/dyly/bloomu/edu/gardenplannerapp/HistoryTableData.java
     private int id, plantHistoryID, workHistoryID, harvestHistoryID;
+=======
+    private int id, plantHistotyID, workHistoryID, harvestHistoryID, bedID;
+>>>>>>> pr/4:app/src/main/java/dyly/bloomu/edu/gardenplannerapp/Database/Database_Objects/HistoryTableData.java
 
     public HistoryTableData()
     {
@@ -13,13 +19,19 @@ public class HistoryTableData {
         this.plantHistoryID = -1;
         this.workHistoryID = -1;
         this.harvestHistoryID = -1;
+        this.bedID = -1;
     }
 
+<<<<<<< HEAD:app/src/main/java/dyly/bloomu/edu/gardenplannerapp/HistoryTableData.java
     public HistoryTableData(int id, int plantHistoryID, int workHistoryID, int harvestHistoryID) {
+=======
+    public HistoryTableData(int id, int plantHistotyID, int workHistoryID, int harvestHistoryID, int bedID) {
+>>>>>>> pr/4:app/src/main/java/dyly/bloomu/edu/gardenplannerapp/Database/Database_Objects/HistoryTableData.java
         this.id = id;
         this.plantHistoryID = plantHistoryID;
         this.workHistoryID = workHistoryID;
         this.harvestHistoryID = harvestHistoryID;
+        this.bedID = bedID;
     }
 
     public int getId() {
@@ -52,5 +64,13 @@ public class HistoryTableData {
 
     public void setHarvestHistoryID(int harvestHistoryID) {
         this.harvestHistoryID = harvestHistoryID;
+    }
+
+    public int getBedID() {
+        return bedID;
+    }
+
+    public void setBedID(int bedID) {
+        this.bedID = bedID;
     }
 }
